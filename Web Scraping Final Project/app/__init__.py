84177b7c04ed2c1b23a026b82e00e6ae36905a20
Suaad Sayyed <suaad.sayyed@mail.utoronto.ca>
2022-10-12 23:25:03 -0400

    Delete Web Scraping Final Project/app directory

--- Web Scraping Final Project/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-webapp = Flask(__name__)
-webapp.config['SECRET_KEY'] = 'you-will-never-guess'
-
-from app import main
-from app import get_result_pages_url
-from app import scrape_job_info
-from app import scrape_job_links
--- Web Scraping Final Project/app/main.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-from app import webapp
-
-
-@webapp.route('/')
-def main():
-    return render_template("main.html")
